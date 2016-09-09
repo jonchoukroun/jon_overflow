2805d3fa3fb07ba5b02659339d64475c304764c8
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-09 13:51:05 -0700

    Implement methods to vote on answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,6 +8,20 @@ class AnswersController < ApplicationController
 		redirect_to question_path(@question)
 	end
 
+	def up_vote
+		@answer = Answer.find(params[:id])
+		@answer.increment!(:up_votes)
+
+    redirect_to questions_path(@question)
+  end
+
+  def down_vote
+		@answer = Answer.find(params[:id])
+		@answer.increment!(:down_votes, by = -1)
+
+		redirect_to questions_path(@question)
+  end
+
 	private
 		def answer_params
 			params.require(:answer).permit(:title, :content)

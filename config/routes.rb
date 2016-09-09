4689e58c9ab10cbdfb435cf3a7a3631556b7bf57
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-09 13:39:24 -0700

    Define patch routes for voting up and down

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   get 'questions/index'
 
   resources :questions do 
+  	member do
+  		patch 'up_vote'
+  		patch 'down_vote'
+  	end
+  	
     resources :answers
+			member do
+	  		patch 'up_vote'
+				patch 'down_vote'
+	  	end
   end
   
   root 'questions#index'

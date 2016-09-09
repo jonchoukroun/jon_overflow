aee7d9efe571ec2ce3e49d2e86e6e3ea99402cfe
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-09 15:31:00 -0700

    Un-comment voting methods on questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -42,19 +42,19 @@ class QuestionsController < ApplicationController
     redirect_to questions_path
   end
 
-  # def up_vote
-  #   @question = Question.find(params[:id])
-  #   @question.increment!(:up_votes)
+  def up_vote
+    @question = Question.find(params[:id])
+    @question.increment!(:up_votes)
 
-  #   redirect_to questions_path
-  # end
+    redirect_to questions_path
+  end
 
-  # def down_vote
-  #   @question = Question.find(params[:id])
-  #   @question.increment!(:down_votes, by = -1)
+  def down_vote
+    @question = Question.find(params[:id])
+    @question.increment!(:down_votes, by = -1)
 
-  #   redirect_to questions_path
-  # end
+    redirect_to questions_path
+  end
 
   private
   	def question_params

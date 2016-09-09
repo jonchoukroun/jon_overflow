48d51d917fff48998e5ba51aee24959d01b234dd
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-09 12:45:30 -0700

    Remove vote counting method from controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -42,16 +42,6 @@ class QuestionsController < ApplicationController
     redirect_to questions_path
   end
 
-  def up_vote
-    @question = Question.find(params[:id])
-    @question.up_vote += 1
-  end
-
-  def down_vote
-    @question = Question.find(params[:id])
-    @question.down_vote -= 1
-  end
-
   private
   	def question_params
   		params.require(:question).permit(:title, :content)

030f3ae004423ddbd31209b5ecccf412a8f07500
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-08 17:00:37 -0700

    Implement index, show, and create methods in Questions controller; implement validation on create method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,18 @@
 class QuestionsController < ApplicationController
   def index
+  	@questions = Question.all
   end
+
+  def show
+  	@question = Question.find(params[:id])
+  end
+  
+  def create
+  	@question = Question.find(question_params)
+  end
+
+  private
+  	def question_params
+  		params.require(:question).permit(:title, :content)
+  	end
 end

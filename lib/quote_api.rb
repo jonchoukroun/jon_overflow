50887f9ece959a1a842a016a757772cab6856f2f
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-12 15:22:42 -0700

    Refactor quote methods to return default quote in case of rate limit

--- lib/quote_api.rb
+++ lib/quote_api.rb
@@ -3,7 +3,17 @@ require 'httparty'
 module QuoteAPI
 	API_URL = "https://api.github.com/zen"
 
-	def self.get_quote
+	# Hard coded quote in case of rate limit
+	def self.standard_quote
+		return "Always shut the door on your way out."
+	end
+
+	# Call github api for random quote
+	def self.api_quote
 		HTTParty.get(API_URL)
+	end
+
+	def self.get_quote
+		return api_quote ? api_quote : standard_quote
 	end
 end

274ef663ca7a824bd3081dc33cd26ebc243bb1e4
Jon Choukroun <jonchoukroun@gmail.com>
2016-09-14 11:59:37 -0700

    Implement new question request from index route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,11 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
     @questions = @questions.sort_by { |q| q.count_votes }.reverse!
+    @question = Question.new
   end
 
   def show
   	@question = Question.find(params[:id])
-  end
-
-  def new
-    @question = Question.new
   end
 
   def edit
@@ -22,7 +19,7 @@ class QuestionsController < ApplicationController
   	@question = Question.create(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to questions_path
     else
       render 'new'
     end

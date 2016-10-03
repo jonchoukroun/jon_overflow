493e9ca9854b5e87257ff8520162d0aa145bbe98
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-02 17:14:07 -0700

    Add sign up and login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  get 'questions/index'
 
+  # Signup routes
+  get '/signup' => 'users#new'
+  post '/user/' => 'users#create'
+
+  # Login/logout routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  # Questions resource that allows for voting up/down
   resources :questions do 
   	member do
   		patch 'up_vote'
   		patch 'down_vote'
   	end
   	
+  # Questions resource that allows for voting up/down
     resources :answers do
 			member do
 	  		patch 'up_vote'

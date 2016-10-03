f59ee28c9035c556e1b372c4b1746eafc8d8ec29
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-02 17:11:50 -0700

    Add belongs_to relationship with user

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,10 @@
 class Answer < ActiveRecord::Base
+  belongs_to :user
   belongs_to :question
-  
-  validates_presence_of :title, :content
+
+  validates_presence_of :content
 
   def count_votes
-    self.up_votes + self.down_votes
+    self.up_votes - self.down_votes
   end
 end

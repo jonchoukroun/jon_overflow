40188371148f1fdce4c9b9071a2e1f05c7350139
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-03 20:03:51 -0700

    Implement toggle method to change new question form link text

--- app/assets/javascripts/application/new_question.js
+++ app/assets/javascripts/application/new_question.js
@@ -1,12 +1,22 @@
 var NewQuestion = {
   bindListeners: function() {
-    $('a.new-question').on('click', this.toggleQuestionForm);
+    $('a.new-question').on('click', this.toggleQuestionForm)
+                      .on('click', this.toggleLinkText);
   },
 
   toggleQuestionForm: function(e) {
     e.preventDefault();
     $('form#new_question').toggleClass('hidden');
-    $(this).html('<p>').append('Back');
+  },
+
+  toggleLinkText: function() {
+    var linkText = $(this);
+    var formStatus = $('form#new_question').attr('class');
+    if (formStatus === 'hidden') {
+      linkText.text('New Question');
+    } else {
+      linkText.text('Back');
+    }
   }
 }
 

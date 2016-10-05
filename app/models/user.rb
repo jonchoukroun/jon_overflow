d495896fb71796cf86094bec8e3c97b8c9b1fffa
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-04 22:13:11 -0700

    Add validations for name, email, and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
+  # Ensure email uniqueness with case insensitivity
+  before_save { self.email = email.downcase }
+  
   has_many :questions
   has_many :answers
-  validates_associated :questions
-  validates_associated :answers
 
-  # User bcrypt for authentication
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,     presence: true, length: { maximum: 50 },
+                       uniqueness: true
+  validates :email,    presence: true, format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false } 
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Use bcrypt for authentication
   has_secure_password
 end

6d2967286966bb894143d9d9ebe82ef87a5ba9cb
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-04 23:05:54 -0700

    Create users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
 end

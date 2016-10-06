b850959a1795ae1b332ab2a852502ff9be36362d
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-05 18:10:38 -0700

    Implement create action and strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,35 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
   def create
+    @user = User.create(user_params)
+
+    respond_to do |format|
+      if @user.save
+        # sessions[:current_user] = @user
+        format.js {render js: "window.location.href = ('#{user_path(@user)}');"}
+      else
+        @message = @user.errors.full_messages
+        format.js {render 'error.js.erb'}
+      end
+    end
   end
 
   def update
   end
   
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

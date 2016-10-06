27a859f8f4905283303b387e2793cbb2543d31b0
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-05 18:17:00 -0700

    Comment out association validations, that don't work

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,9 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
-  validates_associated :user
-  validates_associated :question
+  # Based on questions, this won't work
+  # validates_associated :user
+  # validates_associated :question
   validates_presence_of :content, :user_id, :question_id
 
   def count_votes

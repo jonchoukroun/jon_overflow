46b50a3918fe4fc13b38cd12d2bb21350de121a3
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-06 13:31:54 -0700

    Implement create and destroy routes for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      respond_to do |format|
+        format.js { render js: "window.location.href = ('#{user_path(user)}')" }
+      end
+    else
+      respond_to do |format|
+        if user
+          @message = ["Incorrect username/password combination."]
+          format.js
+        else
+          format.js { render js: "window.location.href = ('#{signup_path}');" }
+        end
+      end
+    end
   end
 
   def destroy
+    log_out
+    redirect_to login_path
   end
 end

db4a7e34742ae27408d733a381045857b0f2e62e
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-07 00:48:04 -0700

    Create category model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+5.times do
+  Category.create!(
+    name: Faker::Commerce.department,
+    description: Faker::Company.catch_phrase
+  )
+end
+
 6.times do
-  User.create(
+  User.create!(
     name: Faker::Superhero.name,
     email: Faker::Internet.email, 
-    password_digest: Faker::Hipster.words(1)
+    password: Faker::Internet.password(6)
   )
 end
 
 12.times do
-  Question.create(
+  Question.create!(
     title: Faker::ChuckNorris.fact,
     content: Faker::Company.catch_phrase + ". " + Faker::Company.catch_phrase,
-    # user_id: rand(1..6)
+    user_id: rand(1..6),
+    category_id: rand(1..5)
   )
 end
 
 24.times do 
-  Answer.create(
+  Answer.create!(
     content: Faker::Hacker.say_something_smart + ".  " + Faker::Hacker.say_something_smart,
-    # user_id: rand(1..6),
-    # question_id: rand(1..12)
+    user_id: rand(1..6),
+    question_id: rand(1..12)
   )
 end

9e03c63bbac42e0ac7fc5f33a53797859d6f78d0
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-07 00:45:35 -0700

    Implement methods to join with questions table and count associated questions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :questions
+  # May not work
   validates_associated :questions
+
+  def get_questions
+    Category.joins(:questions)
+  end
+
+  def count_questions
+    get_questions.count
+  end
 end

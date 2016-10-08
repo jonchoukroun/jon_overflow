670093747b6feb0d336d138ee7bf9210e207d0f6
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-08 13:16:34 -0700

    Simplify index and show actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,12 +2,9 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    # layout '_header'
   end
 
   def show
     @category = Category.find(params[:id])
-    # @questions = @category.get_questions
-    # render 'questions/index'
   end
 end

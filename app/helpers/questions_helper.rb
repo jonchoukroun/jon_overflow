48cbd2134e07efc7af231fc6f37205aecee251cf
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-09 12:39:57 -0700

    Mock up methods to display user name and date created for quesions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,11 @@
 module QuestionsHelper
+  def get_username(question)
+    User.find(question.user_id).name
+  end
+
+  def create_date(question)
+  end
+
   def form_action(question)
     if question.persisted?
       question_path(question)

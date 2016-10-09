04974329507122032b7571ef79ba186fcf96b3a0
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-09 11:01:56 -0700

    Correct object name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @questions = Questions.where("category_id = ?", params[:id])
+    @questions = Question.where("category_id = ?", params[:id])
   end
 end

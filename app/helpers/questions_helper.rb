e5541e690c9cba296cce954c7b4392449d34ba24
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-09 13:37:18 -0700

    Remove username and publish date methods

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,4 @@
 module QuestionsHelper
-  def get_username(question)
-    User.find(question.user_id).name
-  end
-
-  def create_date(question)
-  end
-
   def form_action(question)
     if question.persisted?
       question_path(question)

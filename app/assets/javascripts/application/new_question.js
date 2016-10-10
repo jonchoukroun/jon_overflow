72bfd649d827a3d909bfc629e2dbe1c1b4cc1a53
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-09 18:55:10 -0700

    Fix event listeners to target loaded element, then pass anchor as parameter

--- app/assets/javascripts/application/new_question.js
+++ app/assets/javascripts/application/new_question.js
@@ -1,12 +1,11 @@
 var NewQuestion = {
   bindListeners: function() {
-    $('a.new-question').on('click', this.toggleQuestionForm);
-    $('a.cancel-question').on('click', this.toggleQuestionForm);
+    $('#main-content').on('click', 'a.new-question', this.toggleQuestionForm);
+    $('#main-content').on('click', 'a.cancel-question', this.toggleQuestionForm);
   },
 
   toggleQuestionForm: function(e) {
     e.preventDefault();
-    console.log('fuck');
     $('form#new_question').toggleClass('hidden');
     $('a.new-question').toggleClass('hidden');
     $('.container.error-messages').empty();

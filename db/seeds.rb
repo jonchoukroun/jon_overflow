61e6ce9acce7eb7806ca006ff928e87f40fae20c
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-14 17:38:55 -0700

    Update seeds to production db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-5.times do
-  Category.create!(
-    name: Faker::Commerce.department,
-    description: Faker::Company.catch_phrase
-  )
-end
+Category.create!(
+  name: 'Dining & Night Life',
+  description: 'Enjoy Southern California\'s best restaurants, bars, and numerous watering holes...'
+)
 
-6.times do
-  User.create!(
-    name: Faker::Superhero.name,
-    email: Faker::Internet.email, 
-    password: Faker::Internet.password(6)
-  )
-end
+Category.create!(
+  name: 'Live Events',
+  description: 'Concerts, sporting events, festivals and other happenings...'
+)
 
-12.times do
-  Question.create!(
-    title: Faker::ChuckNorris.fact,
-    content: Faker::Company.catch_phrase + ". " + Faker::Company.catch_phrase,
-    user_id: rand(1..6),
-    category_id: rand(1..5)
-  )
-end
+Category.create!(
+  name: 'Outdoor Activities',
+  description: 'Hiking, Rafting, Rowing, and other fresh-air, blood-pumping experiences...'
+)
 
-24.times do 
-  Answer.create!(
-    content: Faker::Hacker.say_something_smart + ".  " + Faker::Hacker.say_something_smart,
-    user_id: rand(1..6),
-    question_id: rand(1..12)
-  )
-end
+Category.create!(
+  name: 'Must See',
+  description: 'Things not to miss when visiting Southern California...',
+)
+
+Category.create!(
+  name: 'Shopping',
+  description: 'Where to spend that money burning a hole in your pocket.'
+)
+

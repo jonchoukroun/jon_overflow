4ebdf3789ab9cb38ce45ddd89c356718f91840c0
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-14 17:57:32 -0700

    Set passenger restart to touch

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock '3.6.1'
 set :application, 'socal-kin'
 set :repo_url, 'git@github.com:jonchoukroun/socal-kin.git'
 
+set :passenger_restart_with_touch, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 

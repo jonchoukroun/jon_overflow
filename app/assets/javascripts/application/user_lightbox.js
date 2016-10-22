4f747fcd7f2a2dac8f810e481e3baa5026400101
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-21 18:31:07 -0700

    Implement methods to open and close lightbox, and hide other content

---
+++ app/assets/javascripts/application/user_lightbox.js
@@ -0,0 +1,37 @@
+var UserLightbox = {
+  bindListeners: function() {
+    // Lightbox with login form
+    $('body').on('click', 'a.user-login', this.toggleLightbox);
+
+    // Lightbox with signup form
+    $('body').on('click', 'a.user-signup', this.toggleLightbox);
+
+    // Close lightbox
+    $('body').on('click', 'a.close-lightbox', this.closeLightbox);
+
+    // Change from login to signup
+    $('body').on('click', 'a.convert-form', this.convertForm)
+  },
+
+  toggleLightbox: function() {
+    $('#content').toggleClass('hidden');
+    $('#lightbox-container').toggleClass('hidden');
+    $('.intro-buttons').toggleClass('hidden');
+  },
+
+  closeLightbox: function(e) {
+    e.preventDefault();
+    $('#lightbox-container').empty();
+    UserLightbox.toggleLightbox();
+  },
+
+  convertForm: function() {
+    $('#lightbox-container').empty();
+    UserLightbox.toggleLightbox();
+    UserLightbox.toggleLightbox();
+  }
+}
+
+$(document).ready(function() {
+  UserLightbox.bindListeners();
+});

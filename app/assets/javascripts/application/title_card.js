0aa8b0c8ef72556565535aacde03b36498e03d49
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-27 13:48:53 -0700

    Set main-content height on home page to cumulative category height box

--- app/assets/javascripts/application/title_card.js
+++ app/assets/javascripts/application/title_card.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
   // Deduct body top+bottom margins from window height
-  var navMargins = $('body').css('margin').slice(0, 2) * 2;
-  var windowHeight = $(window).height() - navMargins;
+  var navMargin = $('body').css('margin').slice(0, 2);
+  var windowHeight = $(window).height() - navMargin;
   var windowWidth = $(window).width();
-  console.log(windowWidth);
+
+  // Sum height of all county boxes
+  var countyHeights = function() {
+    $('.category-box').each(function() {
+      var sum = 0;
+      return sum += $(this).height();
+    });
+  };
 
   // Set title card to full browser height
   $('.title-card').height(windowHeight);
 
   // Set main content min height to full browser height
-  $('#main-content').css('min-height', windowHeight);
+  $('#main-content').css('min-height', countyHeights);
 
   // Auto scroll to top of categories preview
   $('.title-card').on('click', 'a.scroll-down', function(e) {
     e.preventDefault();
     $('html, body').animate({
-      scrollTop: $('#main-content').offset().top - (navMargins / 2)
+      scrollTop: $('#main-content').offset().top - (navMargin)
     }, 700);
   });
 });

9a12ef597be0d8fd844f38440cebe8010f2b5fbf
Jon Choukroun <jonchoukroun@gmail.com>
2016-10-28 15:00:03 -0700

    Remove navbar compensation when rendering title card and main content

--- app/assets/javascripts/application/title_card.js
+++ app/assets/javascripts/application/title_card.js
@@ -1,26 +1,19 @@
 $(document).ready(function() {
   // Deduct body top+bottom margins from window height
-  var navMargin = $('body').css('margin').slice(0, 2);
-  var windowHeight = $(window).height() - navMargin;
+  var navMargin = parseInt($('body').css('margin').slice(0, 2));
+  var windowHeight = $(window).height();
   var windowWidth = $(window).width();
-
-  // Sum height of all county boxes
-  var countyHeights = function() {
-    $('.category-box').each(function() {
-      var sum = 0;
-      return sum += $(this).height();
-    });
-  };
 
   // Set title card to full browser height
   $('.title-card').height(windowHeight);
 
-  // Set main content min height to full browser height
-  $('#main-content').css('min-height', countyHeights + navMargin);
+  // Set main content min height to full window height
+  $('#main-content').css('min-height', windowHeight);
 
   // Auto scroll to top of categories preview
   $('.title-card').on('click', 'a.scroll-down', function(e) {
     e.preventDefault();
+
     $('html, body').animate({
       scrollTop: $('#main-content').offset().top - navMargin
     }, 700);

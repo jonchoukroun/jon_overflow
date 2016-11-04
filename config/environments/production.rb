60c202cd8c694993ec076df3eb1874803b0ec230
Jon Choukroun <jonchoukroun@gmail.com>
2016-11-04 12:42:27 -0700

    Set env variables used by paperclip and image host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   # Use environment variable instead of secrets.yml
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
+  # Access image hosting service
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :url => ':s3_domain_url',
+    :path => "/:class/:attachment/:id_partition/:style/:filename"
+  }
+
 end

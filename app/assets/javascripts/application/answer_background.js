eb0ab5b97525e11414d7c102c349644e25debdd9
Jon Choukroun <jonchoukroun@gmail.com>
2016-11-04 19:26:50 -0700

    Toggle image visible or darken on answer click

--- app/assets/javascripts/application/answer_background.js
+++ app/assets/javascripts/application/answer_background.js
@@ -1,6 +1,33 @@
+var clear = 'rgba(0,0,0,0)';
+var dark = 'rgba(0,0,0,0.7)';
+
+showImage = function(answer, url) {
+    $(answer).css({
+      'background': 'linear-gradient('+clear+', '+clear+'),' +url,
+      'color': clear
+    });
+};
+
+darkenImage = function(answer, url) {
+    $(answer).css({
+      'background': 'linear-gradient('+dark+', '+dark+'),' +url,
+      'color': 'white'
+    });
+};
+
 $(document).ready(function() {
   // Target full-answer
   $('#main-content').on('click', '.full-answer', function(e) {
-    e.preventDefault();
+    // Get background image url
+    var background = $(this).css('background').split(' ');
+    console.log(background);
+    var url = background[20];
+
+    // Toggle image visibility
+    if (background[3] === '0),') {
+      darkenImage(this, url);
+    } else {
+      showImage(this, url);
+    }
   });
 });

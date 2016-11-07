14bd47eb2b6a8e4ed90c03ce60f937af52233efb
Jon Choukroun <jonchoukroun@gmail.com>
2016-11-06 16:50:57 -0800

    Set answer background size and repeat settings

--- app/assets/javascripts/application/answer_background.js
+++ app/assets/javascripts/application/answer_background.js
@@ -1,16 +1,23 @@
+// Globals for background image settings
 var clear = 'rgba(0,0,0,0)';
 var dark = 'rgba(0,0,0,0.7)';
 
+// lighten background filter and hide text
 showImage = function(answer, url) {
     $(answer).css({
       'background': 'linear-gradient('+clear+', '+clear+'),' +url,
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat',
       'color': clear
     });
 };
 
+// Return to default, obstruct image & show text
 darkenImage = function(answer, url) {
     $(answer).css({
       'background': 'linear-gradient('+dark+', '+dark+'),' +url,
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat',
       'color': 'white'
     });
 };
@@ -20,7 +27,6 @@ $(document).ready(function() {
   $('#main-content').on('click', '.full-answer', function(e) {
     // Get background image url
     var background = $(this).css('background').split(' ');
-    console.log(background);
     var url = background[20];
 
     // Toggle image visibility
